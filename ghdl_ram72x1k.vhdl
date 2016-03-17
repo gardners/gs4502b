7457d1829345663774994efbe58850e2cc355947
gardners <paul@servalproject.org>
2016-03-17 20:47:08 +1030

    adjust GHDL simulation I-CACHE RAM to match interface of Xilinx IP Core for synthesis.

--- ghdl_ram72x1k.vhdl
+++ ghdl_ram72x1k.vhdl
@@ -7,57 +7,42 @@ use Std.TextIO.all;
 use work.debugtools.all;
 use work.icachetypes.all;
 
-ENTITY ram72x1k IS
+ENTITY icache_ram IS
   PORT (
     clka : IN STD_LOGIC;
-    addra : IN unsigned(9 DOWNTO 0);
-    douta : OUT std_logic_vector(71 downto 0);
-    ena : in std_logic;
-    wea : in std_logic;
-    dina : in std_logic_vector(71 downto 0);
+    wea : IN STD_LOGIC;
+    addra : IN std_logic_vector(9 DOWNTO 0);
+    dina : IN std_logic_vector(71 downto 0);
     clkb : IN STD_LOGIC;
-    web : IN STD_LOGIC;
-    addrb : IN unsigned(9 DOWNTO 0);
-    dinb : IN std_logic_vector(71 downto 0)
+    addrb : IN std_logic_vector(9 DOWNTO 0);
+    doutb : OUT std_logic_vector(71 downto 0)
     );
-END ram72x1k;
+END icache_ram;
 
-architecture behavioural of ram72x1k is
+architecture behavioural of icache_ram is
 
   type ram_t is array (0 to 1023) of std_logic_vector(71 downto 0);
   signal ram : ram_t;
-
-  signal douta_drive : std_logic_vector(71 downto 0);
   
 begin  -- behavioural
 
   process(clka,addra,ram)
   begin
-    douta <= douta_drive;
-    douta_drive <= ram(to_integer(addra));
+    if(rising_edge(Clka)) then 
+      doutb <= ram(to_integer(unsigned(addrb)));
+    end if;
     
     report "I-CACHE: A Reading from $" & to_hstring(unsigned(addra));
 --      & " = $" & to_hstring(ram(to_integer(unsigned(addra))));
 
     if(rising_edge(Clka)) then 
-      if ena='1' then
-        if(wea='1') then
-          ram(to_integer(unsigned(addra(9 downto 0)))) <= dina;
-          report "COLOURRAM: A writing to $" & to_hstring(unsigned(addra))
-            & " = $" & to_hstring(dina);
-        end if;
+      if(wea='1') then
+        ram(to_integer(unsigned(addra(9 downto 0)))) <= dina;
+        report "COLOURRAM: A writing to $" & to_hstring(unsigned(addra))
+          & " = $" & to_hstring(dina);
       end if;
     end if;
 
-  end process;
-
-  process (clkb,addrb,ram)
-  begin
-    if(rising_edge(Clkb)) then 
-      if(web='1') then
-        ram(to_integer(addrb)) <= dinb;
-      end if;
-    end if;
   end process;
 
 end behavioural;
--- gs4502b.vhdl
+++ gs4502b.vhdl
@@ -16,46 +16,40 @@ END gs4502b;
 
 architecture behavioural of gs4502b is
 
-  signal icache_lookup_line : unsigned(9 downto 0);
+  signal icache_lookup_line : std_logic_vector(9 downto 0);
   signal icache_read_data : std_logic_vector(71 downto 0);
 
   signal reg_pc : unsigned(15 downto 0);
 
   signal expected_icache_address : unsigned(27 downto 10) := "111100001111000011";
   
-  -- declare components here
-  component ram72x1k is
+  component icache_ram IS
   PORT (
-    clka : IN STD_LOGIC;
-    addra : IN unsigned(9 DOWNTO 0);
-    douta : OUT std_logic_vector(71 downto 0);
-    ena : in std_logic;
-    wea : in std_logic;
-    dina : IN std_logic_vector(71 downto 0);
-    clkb : IN STD_LOGIC;
-    web : IN STD_LOGIC;
-    addrb : IN unsigned(9 DOWNTO 0);
-    dinb : IN std_logic_vector(71 downto 0)
-    );
-    end component;
+      clka : IN STD_LOGIC;
+      wea : IN STD_LOGIC;
+      addra : IN std_logic_vector(9 DOWNTO 0);
+      dina : IN std_logic_vector(71 downto 0);
+      clkb : IN STD_LOGIC;
+      addrb : IN std_logic_vector(9 DOWNTO 0);
+      doutb : OUT std_logic_vector(71 downto 0)
+      );
+    END component;
+
   
 begin  -- behavioural
 
-  icacheram : ram72x1k
+  icacheram : icache_ram
     port map (
       -- CPU READ interface to I-CACHE
-      clka => cpuclock,
-      addra => icache_lookup_line,
-      douta => icache_read_data,
-      ena => '0',
-      wea => '0',
-      dina => (others => '0'),
+      clkb => cpuclock,
+      addrb => icache_lookup_line,
+      doutb => icache_read_data,
 
       -- MMU write interface to I-CACHE
-      clkb => cpuclock,
-      web => '0',
-      addrb => (others => '0'),
-      dinb => icache_read_data
+      clka => cpuclock,
+      wea => '0',
+      addra => (others => '0'),
+      dina => icache_read_data
       );
   
   process(cpuclock, icache_read_data)
@@ -65,7 +59,7 @@ begin  -- behavioural
     end if;
 
     if(rising_edge(cpuclock)) then
-      icache_lookup_line(9 downto 0) <= reg_pc(9 downto 0);
+      icache_lookup_line(9 downto 0) <= std_logic_vector(reg_pc(9 downto 0));
       if std_logic_vector(expected_icache_address)
         = icache_read_data(17 downto 0) then
         reg_pc <= reg_pc + 1;
